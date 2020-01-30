4c9693352d301935c8fd8604962865baf07a7bae
Ken Welsh <ken.w.welsh@gmail.com>
2020-01-29 16:48:57 -0800

    Adding the election analysis.

--- PyPoll.py
+++ PyPoll.py
@@ -6,12 +6,12 @@ import os
 file_to_load = os.path.join("Resources/election_results.csv")
 
 # Assign a variable to save the file to a path.
-file_to_save = os.path.join("Aanalysis", "election_analysis.txt")
+file_to_save = os.path.join("Analysis", "election_analysis.txt")
 
 # Initialize a total vote counter.
 total_votes = 0
 
-# Candidate options and candidate votes
+# Candidate options and candidate votes.
 candidate_options = []
 candidate_votes = {}
 
@@ -37,7 +37,7 @@ with open(file_to_load) as election_data:
         # Get the candidate name from each row.
         candidate_name = row[2]
         
-        # If the candidate does not match any existing candidate add it the
+        # If the candidate does not match any existing candidate, add the
         # the candidate list.
         if candidate_name not in candidate_options:
             
@@ -47,38 +47,58 @@ with open(file_to_load) as election_data:
             # And begin tracking that candidate's voter count.
             candidate_votes[candidate_name] = 0
         
-        # Add a vote to that candidate's count
+        # Add a vote to that candidate's count.
         candidate_votes[candidate_name] += 1
 
-for candidate in candidate_votes:
+# Save the results to text file.
+with open(file_to_save, "w") as txt_file:
     
-    # Retrieve vote count and percentage.
-    votes = candidate_votes[candidate]
-    vote_percentage = float(votes) / float(total_votes) * 100
+    # Print the final vote count to the terminal.
+    election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
     
-    # Print each candidate, their voter count, and percentage to the
-    # terminal.
-    print(f"{candidate}: {vote_percentage:.1f}% ({votes:,})\n")
+    # Save the final vote count to the text file.
+    txt_file.write(election_results)
+    for candidate in candidate_votes:
+        
+        # Retrieve vote count and percentage.
+        votes = candidate_votes[candidate]
+        vote_percentage = float(votes) / float(total_votes) * 100
+        candidate_results = (
+            f"{candidate}: {vote_percentage:.1f}% ({votes:,})\n")
 
-    # Determine winning vote count, winning percentage, and candidate.
-    if (votes > winning_count) and (vote_percentage > winning_percentage):
-        winning_count = votes
-        winning_candidate = candidate
-        winning_percentage = vote_percentage
-
-# Print the winning candidates' results to the terminal.
-winning_candidate_summary = (
-    f"-------------------------\n"
-    f"Winner: {winning_candidate}\n"
-    f"Winning Vote Count: {winning_count:,}\n"
-    f"Winning Percentage: {winning_percentage:.1f}%\n"
-    f"-------------------------\n")
-
-print(winning_candidate_summary)
+        # Print each candidate's voter count and percentage to the terminal.
+        print(candidate_results)
+        
+        #  Save the candidate results to the text file.
+        txt_file.write(candidate_results)
+        
+        # Determine winning vote count, winning percentage, and winning candidate.
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            winning_count = votes
+            winning_candidate = candidate
+            winning_percentage = vote_percentage
+    
+    # Print the winning candidate's results to the terminal.
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+    print(winning_candidate_summary)
+    
+    # Save the winning candidate's results to the text file.
+    txt_file.write(winning_candidate_summary)
 
 # Close the data file
 election_data.close()
 
 # Close the results file
+txt_file.close()
 
 
